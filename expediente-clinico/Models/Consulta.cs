87bd0e7607458360d441034b9bc9dcafa6b20c69
Legend <mejiadominguez00@gmail.com>
2022-03-28 01:29:05 -0700

    Revert "Revert "Se agregarón 3 modelos""
    This reverts commit 87dc288ad67db9c9ea61015b37df61160cc4243d.

---
+++ expediente-clinico/Models/Consulta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace expediente_clinico.Models
+{
+    public class Consulta
+    {
+        public long Id { get; set; }
+        public Medico Medico { get; set; }
+        public Paciente Paciente { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Diagnostico { get; set; }
+        public bool Estatus { get; set; }
+    }
+}
--- expediente-clinico/Models/Especialidad.cs
+++ expediente-clinico/Models/Especialidad.cs
@@ -8,7 +8,7 @@ namespace expediente_clinico.Models
     public class Especialidad
     {
         public long Id { get; set; }
-        public String Nombre { get; set; }
-        public String Descripcion { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
     }
 }
